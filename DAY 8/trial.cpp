831ad665ab7c84d4b39d1028d25cb8308219afcd
Satyajit Panigrahi <satyapani999@gmail.com>
2021-01-03 09:27:14 +0530

    Add files via upload

---
+++ DAY 8/trial.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    vector<int> b;
+    int n;
+    cin>>n;
+    while(n>0)
+    {
+        int t;
+        cin>>t;
+        b.push_back(t);
+        n--;
+    }
+    int c=0;
+    for(auto x:b)
+    {
+        int pro=1;
+        int temp=x;
+        int p=0;
+        while(temp>0)
+        {
+            int d=temp%10;
+            if(d==1)
+                p++;
+            if(d>1)
+                c++;
+        }
+        if(p>1)
+        {
+            cout<<"not barua number";
+            break;
+        }
+        else if(c>1)
+        {
+            cout<<"invalid input";
+            break;
+        }
+        else
+        {
+            
+            for(auto j:b)
+            {
+                pro=pro*j;
+            }
+        }
+        cout<<pro;
+    }
+    
+}
